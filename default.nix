5ae74410216e6fb9465e1a6d1d6c843773dc1543
technoY2K <cloud@kevia.me>
2022-03-10 14:30:34 -0700

    chore: add block-frost-client dep

--- default.nix
+++ default.nix
@@ -13,6 +13,7 @@ let
             hlint
             p.ihp
             mmark
+            blockfrost-client
         ];
         otherDeps = p: with p; [
             # Native dependencies, e.g. imagemagick
