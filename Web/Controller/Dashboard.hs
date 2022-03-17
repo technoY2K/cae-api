d8afd9185741a2c87eed5f22e5b791a8bddad385
technoY2K <cloud@kevia.me>
2022-03-17 15:58:59 -0700

    feat: check session for user

--- Web/Controller/Dashboard.hs
+++ Web/Controller/Dashboard.hs
@@ -1,8 +1,25 @@
 module Web.Controller.Dashboard where
 
-import           Web.Controller.Prelude
-import           Web.View.Dashboard.Index
+import           BasicPrelude             (Maybe (Just, Nothing),
+                                           Monad (return), Text, encodeUtf8,
+                                           ($), (.))
+import qualified Data.ByteString.Lazy     as BL
+import           Web.Controller.Prelude   (Controller (action),
+                                           DashboardController (..),
+                                           Maybe (Just, Nothing),
+                                           Monad (return), User' (email),
+                                           currentUserOrNothing, get, render,
+                                           renderPlain, ($), (.))
+import           Web.View.Dashboard.Index (IndexView (IndexView))
 
 instance Controller DashboardController where
-    action DashboardAction = render IndexView
+    action DashboardAction = do
+        case currentUserOrNothing of
+            Just currentUser -> renderPlain $ convert (get #email currentUser)
 
+            Nothing          -> renderPlain "Go login first"
+
+        render IndexView
+
+convert :: Text -> BL.ByteString
+convert = BL.fromChunks . return . encodeUtf8
