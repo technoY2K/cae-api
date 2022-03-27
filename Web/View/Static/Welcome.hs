2180bc37f89beef41e680c562e7143e4dac01b0a
technoY2K <cloud@kevia.me>
2022-03-27 14:55:25 -0700

    feat: add input form for policy GET request

--- Web/View/Static/Welcome.hs
+++ Web/View/Static/Welcome.hs
@@ -5,11 +5,22 @@ data WelcomeView = WelcomeView
 
 instance View WelcomeView where
     html WelcomeView = [hsx|
-         <div style="background-color: #2a71d0; padding-top: 2rem; padding-bottom: 2rem; color:hsla(196, 13%, 96%, 1); border-radius: 4px">
-              <div style="max-width: 800px; margin-left: auto; margin-right: auto">
-                  <h2 style="margin-top: 0; margin-bottom: 0rem; font-weight: 900; font-size: 3rem">
-                      Cardano Asset Explorer
-                  </h2>
-              </div>
-         </div>
-|]
+            <div style="background-color: #2a71d0; padding-top: 2rem; padding-bottom: 2rem; color:hsla(196, 13%, 96%, 1); border-radius: 4px">
+                <div style="max-width: 800px; margin-left: auto; margin-right: auto">
+                    <h2 style="margin-top: 0; margin-bottom: 0rem; font-weight: 900; font-size: 3rem">
+                        Cardano Asset Explorer
+                    </h2>
+                </div>
+            </div>
+            <div>
+                {renderForm}
+            </div>
+        |]
+        where
+            renderForm = [hsx|
+                <form id="main-form" method="GET" action={DashboardAction}>
+                    <input type="text" name="policyid" class="form-control"/>
+                    <input type="submit" class="btn btn-primary"/>
+                </form>
+                |]
+
