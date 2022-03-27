6afd48677df515584589c483243ac80d33cedea8
technoY2K <cloud@kevia.me>
2022-03-26 20:13:36 -0700

    fix: do not mess with the schemas

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE users (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     email TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
-    locked_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    locked_at TIMESTAMP WITH TIME ZONE DEFAULT NULL,
     failed_login_attempts INT DEFAULT 0 NOT NULL
 );
 ALTER TABLE comments ADD CONSTRAINT comments_ref_post_id FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE;
