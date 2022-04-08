b22a80c484ca3108de3b53e59217c233d95b26d8
technoY2K <cloud@kevia.me>
2022-04-07 20:07:59 -0700

    feat: successfully print out stake id that is passed in

--- Web/Controller/Wallets.hs
+++ Web/Controller/Wallets.hs
@@ -1,7 +1,11 @@
 module Web.Controller.Wallets where
 
 import           Blockfrost.Client
+import           Data.Text
+import           Debug.Trace
 import           Web.Controller.Prelude
 
 instance Controller WalletsController where
-    action ShowWalletAction = renderPlain "Test"
+    action ShowWalletAction = do
+        let si = param @Text "stakeid"
+        trace (unpack si) (renderPlain "test")
