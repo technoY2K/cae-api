d5290383bc52519b41f8e6e01fd1489c88d63710
technoY2K <cloud@kevia.me>
2022-04-07 20:38:30 -0700

    chore: save point

--- Web/Controller/Wallets.hs
+++ Web/Controller/Wallets.hs
@@ -1,11 +1,30 @@
 module Web.Controller.Wallets where
 
 import           Blockfrost.Client
-import           Data.Text
+import           Data.ByteString.Lazy   as BL
+import           Data.Text              as T
+import           Data.Text.Encoding     as T
 import           Debug.Trace
 import           Web.Controller.Prelude
 
 instance Controller WalletsController where
     action ShowWalletAction = do
-        let si = param @Text "stakeid"
-        trace (unpack si) (renderPlain "test")
+        project <- projectFromFile ".blockfrost"
+        result <- runBlockfrost project $ do
+                let si = param @Text "stakeid"
+                getAccount (Address si)
+
+        case result of
+            Left e -> renderPlain $ BL.fromChunks . return . T.encodeUtf8 $ (parseBFError e)
+            Right a -> do
+                let isActive = _accountInfoActive a
+                trace (T.unpack $ show isActive) renderPlain "test"
+
+parseBFError :: BlockfrostError -> Text
+parseBFError b = case b of
+    BlockfrostError t        -> t
+    BlockfrostBadRequest t   -> t
+    BlockfrostTokenMissing t -> t
+    BlockfrostFatal t        -> t
+    _                        -> "Unknown error"
+
